04cdbab95c0598156b784ff40a424b9963004854
johromro <johromro@gmail.com>
2015-07-06 07:17:46 -0600

    Segunda version

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 var numero = Math.random();
 
+var str = ' MAYOR que 0.5'
+
 if (numero <= 0.5){
-  console.log('\n '+numero+ 'Menor que 0.5 \n');
-}else{
-  console.log('\n '+numero+ 'Mayor que 0.5 \n')
+  str = ' Menor que 0.5 ';
 }
+
+console.log('\n '+numero+ str+ ' \n')
