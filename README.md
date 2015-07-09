f885d96e11c5b44a3082096acd3e0b7b591bba70
johromro <johromro@gmail.com>
2015-07-09 07:44:36 -0600

    Commit de ejercicio

---
+++ README.md
@@ -0,0 +1,2 @@
+Este es un proyecto de prueba, para realizar ejercicios de un curso
+https://www.miriadax.net
--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 var numero = Math.random();
 
-var str = ' MAYOR que 0.5'
+var str = ' MAYOR que 0.5';
 
 if (numero <= 0.5){
   str = ' Menor que 0.5 ';
 }
 
-console.log('\n '+numero+ str+ ' \n')
+console.log('\n '+numero+ str+ ' \n');
